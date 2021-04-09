7f77ad2223a4e314cd7661172192d11dc0d4ce69
yehnet <yehnet@post.bgu.ac.il>
2021-04-09 17:20:11 +0300

    Acceptance Tests - init

---
+++ Logic/Tests/AcceptanceTests.test.ts
@@ -0,0 +1,21 @@
+import {} from "../Users/Register";
+import {} from "../System";
+import { expect } from "chai";
+
+describe('Visitor:', () => {
+    // it('2.1: Enter - enter to the system as a guest', () => {
+
+    // });
+
+    // it('2.2: Exit - exit', () => {
+
+    // });
+
+    it('2.3: Registration - Add a new user to the system.', () => {
+        // let reg = register("Test@test.com", "TESTER");
+        // expect(reg).to.be.true;
+        // reg = register("Test@test.com", "TESTER");
+        // expect(reg).to.be.false;
+        // expect(System.users.contains("Test@test.com")).to.be.true;
+    });
+});
