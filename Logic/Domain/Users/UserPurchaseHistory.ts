1766a36781c04d019dc7100576f4289e6956a7e9
Mark Oulitin <fragoric@gmail.com>
2021-04-24 20:01:33 +0300

    Added association class between shop and purchase

--- Logic/Domain/Users/UserPurchaseHistory.ts
+++ Logic/Domain/Users/UserPurchaseHistory.ts
@@ -2,21 +2,22 @@ import {Purchase} from "../ProductHandling/Purchase";
 import {UserHistoryNotFound} from "../ProductHandling/ErrorMessages";
 
 
-type history_info = {purchase_id:number, shop_id:number, purchase:Purchase};
-type history = {user_id: number, history_info: history_info[]};
+type history_key = {user_id: number, shop_id: number, purchase_id: number};
+type history_entry = {key: history_key, purchase:Purchase};
 export interface UserPurchaseHistory{
-    history:history[]
+    history:history_entry[]
     addPurchase(user_id: number, purchase: Purchase): void
     getUserPurchases(user_id: number): Purchase[] | string
+    getShopPurchases(shop_id: number): Purchase[] | string
 }
 export class UserPurchaseHistoryImpl implements UserPurchaseHistory{
     private static instance: UserPurchaseHistoryImpl;
-    private readonly _history: history[];
+    private readonly _history: history_entry[];
     private constructor() {
         this._history = []
     }
 
-    get history(): history[] {
+    get history(): history_entry[] {
         return this._history;
     }
 
@@ -27,24 +28,23 @@ export class UserPurchaseHistoryImpl implements UserPurchaseHistory{
     }
 
     public addPurchase(user_id: number, purchase: Purchase): void {
-        const info = {
-            purchase_id: purchase.order_id,
-            shop_id: purchase.shop.shop_id,
+        this._history.push({
+            key: {
+                user_id: user_id,
+                purchase_id: purchase.order_id,
+                shop_id: purchase.shop.shop_id
+            },
             purchase: purchase
-        }
-        const value = this._history.find(history => history.user_id == user_id);
-        if(value == undefined)
-            this._history.push({
-                user_id:user_id,
-                history_info: [info]
-            })
-        else value.history_info.push(info)
+        })
     }
 
     public getUserPurchases(user_id: number): Purchase[] | string {
-        const value = this._history.find(history => history.user_id == user_id)
-        if (value === undefined)
-            return UserHistoryNotFound
-        return value.history_info.map((info: history_info) => info.purchase);
+        return this._history.reduce((acc: Purchase[], entry: history_entry) =>
+            entry.key.user_id == user_id ? acc.concat(entry.purchase) : acc, []);
+    }
+
+    getShopPurchases(shop_id: number): Purchase[] | string {
+        return this._history.reduce((acc: Purchase[], entry: history_entry) =>
+            entry.key.shop_id == shop_id ? acc.concat(entry.purchase) : acc, []);
     }
 }
