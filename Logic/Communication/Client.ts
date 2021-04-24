8e73c6777e2108b98d9ee733e561a70a98696a1b
Mark Oulitin <fragoric@gmail.com>
2021-04-24 10:51:01 +0300

    Creating Server.ts and Client.ts

---
+++ Logic/Communication/Client.ts
@@ -0,0 +1,12 @@
+import WebSocket = require("ws");
+const socket = new WebSocket('ws://localhost:8080');// connect
+
+// Connection opened
+socket.addEventListener('open', function (event) {
+    socket.send('Hello Server!');
+});
+
+// Listen for messages
+socket.addEventListener('message', function (event) {
+    console.log('Message from server ', event.data);
+});
