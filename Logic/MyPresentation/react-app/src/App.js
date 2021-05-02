3be7a59fb5e049d703b4bdcde69e1eda21c47a00
Lior Pevzner <liorpev@post.bgu.ac.il>
2021-05-02 18:51:15 +0300

    Login and Register components ready to use

---
+++ Logic/MyPresentation/react-app/src/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+//import Register from './components/Register'
+import Login from './components/Login'
+class App extends Component{
+    render(){
+    return (
+      //<Register/>
+      <Login/>
+    );
+  }
+}
+export default App;
---
+++ Logic/MyPresentation/react-app/src/components/Login.jsx
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import Reactlogo from './images/login.jpg'
+class Login extends Component {
+state = {
+    email:'',
+    password:''
+};
+
+handleSubmit = (event) =>{
+    console.log(this.state);
+    event.preventDefault();
+}
+handleUserEmail = (event) =>{
+    this.setState({email:event.target.value});
+}
+handlePassword = (event) =>{
+    this.setState({password:event.target.value});
+}
+    render() {
+        return(
+        <div className="wrapper fadeInDown">
+            <div id="formContent">
+                <div className="fadeIn first">
+      <             img src={Reactlogo} id="icon" alt="User Icon" />
+                </div>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" id="login" className="fadeIn second" name="login" placeholder="example@example.com" onChange={this.handleUserEmail}/>
+                    <input type="text" id="password" className="fadeIn third" name="login" placeholder="Password" onChange={this.handlePassword}/>
+                    <input type="submit" className="fadeIn fourth" value="Login"/>
+                </form>
+            </div>
+        </div>
+        );
+    }
+}
+export default Login;
