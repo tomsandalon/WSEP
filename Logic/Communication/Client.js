038d881091a5696cea411a4a8b8644fdbd5f2c4b
Mark Oulitin <fragoric@gmail.com>
2021-05-03 10:56:25 +0300

    Cookies added

--- Logic/Communication/Client.js
+++ Logic/Communication/Client.js
@@ -3,30 +3,6 @@ const {sleep} = require("async-parallel");
 const fs = require('fs')
 const path = require('path')
 const https = require('https')
-
-// import {sleep} from "async-parallel";
-// const socket = new WebSocket('ws://localhost:8080');// connect
-//
-// // Connection opened
-// socket.addEventListener('open', function (event) {
-//     socket.send('Hello Server!');
-// });
-//
-// // Listen for messages
-// socket.addEventListener('message', function (event) {
-//     console.log('Message from server ', event.data);
-// });
-// const id = Math.random()
-// const ws = new WebSocket('ws://localhost:8000');
-//
-// ws.on('open', function open() {
-//     ws.send(`Hi I am ${id}`);
-// });
-// ws.on('message', function incoming(data) {
-//     console.log(data);
-//     sleep(1000).then(_ => ws.send(`Hey ${id}`))
-// });
-// console.log(`Client ${id} successful boot`)
 const config_path = "./Logic/Communication/Config/";
 const key_file = "server_key.pem";
 const cert_file = "server_cert.pem";
@@ -40,16 +16,31 @@ const options = {
     cert: fs.readFileSync(config_path + cert_file, "utf8"),
     requestCert: true,
     rejectUnauthorized: true,
-
+    Cookie: undefined,
     ca: [ fs.readFileSync(config_path + ca_file) ],
 }
-
-const req = https.request(options, function (res) {
-    console.log('STATUS: ' + res.statusCode);
-    console.log('HEADERS: ' + JSON.stringify(res.headers));
-    res.setEncoding('utf8');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+// const app = express();
+// app.use(cookieParser());
+const req = https.request(options);
+req.on('response', function (res) {
+    const cookies = getAllCookies(res.rawHeaders);
+    console.log(cookies);
     res.on('data', function (chunk) {
         console.log('BODY: ' + chunk);
     });
 });
 req.end()
+
+function getAllCookies (cookies){
+    console.log(cookies)
+    let result = [];
+    for (let i = 0; i < cookies.length; i++) {
+        if(cookies[i] === 'Set-Cookie'){
+            result.push(cookies[i+1].split(';', 1)[0].split('='))
+            i++;
+        }
+    }
+    return result;
+}
--- Logic/Communication/Server.ts
+++ Logic/Communication/Server.ts
@@ -6,7 +6,10 @@ import {sleep} from "async-parallel";
 import {System} from "../../Tests/AcceptanceTests/System";
 import {SystemDriver} from "../../Tests/AcceptanceTests/SystemDriver";
 import {Service} from "../Service/Service";
-const app = require('express');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const app = express();
+app.use(cookieParser());
 const host = 'localhost'
 const port = 8000
 const config_path = "./Logic/Communication/Config/";
@@ -17,7 +20,23 @@ const options = {
     cert: fs.readFileSync(config_path + cert_file, "utf8"),
 };
 //initialize a https server
-const server = https.createServer(options, (req, res) =>{
-    res.writeHead(200)
-    res.end("My first server!\n" + req.url + "\n:D")
+const server = https.createServer(options, app);
+app.get('/', (req: any, res: any) => {
+    res.cookie('foo', 'bar', {})
+    res.cookie('jonny', 'sins', {})
+    res.send("My first server!\n" + req.url + "\n:D")
+})
+// server.on('request', (req, res) =>{
+//     console.log("REQ " + req.getHeader('Set-Cookie'))
+//     // res.setHeader('set-cookie', 'foo=bar')
+//     // To Write a Cookie
+//     // res.cookie.cookie = "name=mark"
+//     // res.setHeader('Cookie', ['type=ninja', 'language=javascript']);
+//     res.writeHead(200);
+//     res.end("My first server!\n" + req.url + "\n:D")
+// })
+
+//start our server
+server.listen( port,() => {
+    console.log(`Server is running on port ${port}`);
 });
