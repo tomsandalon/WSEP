3b8dfa5791764faacbcec103286b7ea5770130b3
Lior Pevzner <liorpev@post.bgu.ac.il>
2021-05-04 13:45:18 +0300

    Payment component ready

--- Logic/MyPresentation/react-app/src/components/Payment.jsx
+++ Logic/MyPresentation/react-app/src/components/Payment.jsx
@@ -1,41 +1,51 @@
 import React, { Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Reactlogo from './images/payment.png'
 class Payment extends Component {
+
+    state = {
+        name:'',
+        cardNo:'',
+        mm:'',
+        yy:'',
+        cvv:''
+
+    }
+    handleName = (event) => {this.setState({name:event.target.value});} 
+    handleCard = (event) => {this.setState({cardNo:event.target.value});}
+    handleMM = (event) => {this.setState({mm:event.target.value});}
+    handleYY = (event) => {this.setState({yy:event.target.value});}
+    handleCVV = (event) => {this.setState({cvv:event.target.value});}
+    handleSubmit = (event) => 
+    {
+        console.log(this.state);
+    }    
+    on
     render() {
         return (
-    <div className="card">
-        <div className="card-body">
-            <h4 className="card-title mb-4">Payment info</h4>
-            <form role="form">
-      <div className="form-group">
-      <label for="username">Name on card</label>
-      <input type="text" className="form-control" name="username" placeholder="Ex. John Smith" required=""/>
-      </div> 
-      
-      <div className="form-group">
-      <label for="cardNumber">Card number</label>
-      <div className="input-group">
-          <input type="text" className="form-control" name="cardNumber" placeholder=""/>
-          <div className="input-group-append">
-              <span className="input-group-text">
-                  <i className="fab fa-cc-visa"></i> &nbsp; <i className="fab fa-cc-amex"></i> &nbsp; 
-                  <i className="fab fa-cc-mastercard"></i> 
-              </span>
-          </div>
-      </div> 
-      </div> 
-      <div className="row">
-            <figure className="itemside mb-4">
-                <input type="text" className="form-control" placeholder="MM"/>
-                <input type="text" className="form-control" placeholder="YY"/>
-                <input type="text" className="form-control" placeholder="CVV"/>
-            </figure>
-      </div> 
-      <button className="subscribe btn btn-primary btn-block" type="button"> Confirm  </button>
-      </form>
-            </div> 
-          </div>  
+            <div className="wrapper fadeInDown" href="#register">
+            <div id="formContent">
+                <div className="fadeIn first">
+                <img src={Reactlogo} id="icon" alt="User Icon" />
+                </div>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" className="fadeIn second" placeholder="Ex. John Smith" onChange={this.handleName}/>
+                    <input type="text" className="fadeIn third" placeholder="Card No." onChange={this.handleCard}/>
+                    <div className="row">
+                        <div className="col-4">
+                        <input type="text" className="fadeIn third" placeholder="MM" onChange={this.handleMM} />
+                        </div>
+                        <div className="col-4">
+                        <input type="text" className="fadeIn third" placeholder="YY" onChange={this.handleYY}/>
+                        </div>
+                        <div className="col-4">
+                        <input type="text" className="fadeIn third" placeholder="CVV" onChange={this.handleCVV}/>
+                        </div>
+                    </div>
+                    <input type="submit" className="fadeIn fourth" value="Confirm"/>
+                </form>
+            </div>
+        </div> 
         );
     }
 }
