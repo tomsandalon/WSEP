e92c8c1ef44769ba1e6916009ca74e5fc76f2cb3
Tomer <nagarcode@gmail.com>
2021-05-08 13:49:52 +0300

    first com

--- Logic/MyPresentation/react-app/src/App.js
+++ Logic/MyPresentation/react-app/src/App.js
@@ -1,26 +1,30 @@
-import React, {Component} from 'react';
-import Register from './components/Register'
-import Login from './components/Login'
-import Navigation from './components/Navbar/Navigation'
-import Home from './components/Home';
-import ShopItems from './components/ShopItems';
-import FiltersItems from './components/FiltersItems';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Payment from './components/Payment';
+import React, { Component } from "react";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Navigation from "./components/Navbar/Navigation";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Payment from "./components/Payment";
+import Stores from "./pages/Stores";
+import "./App.css";
+import AddManager from "./pages/Add_Manager";
 
-class App extends Component{
-  handleLogout = () =>{
+class App extends Component {
+  handleLogout = () => {
     console.log("Logged out");
-  }
-    render(){
+  };
+  render() {
     return (
       <Router>
         <div className="app">
-          <Navigation handleLogout={this.handleLogout}/>
+          <Navigation handleLogout={this.handleLogout} />
           <Switch>
-            <Route path="/" exact component={ShopItems}/>
+            {/* <Route path="/" exact component={ShopItems}/> */}
+            <Route path="/" exact component={Stores} />
+            <Route path="/addmanager/:storeID">
+              <AddManager />
+            </Route>
             <Route path="/login" component={Login} />
-            <Route path="/payment" component={Payment}/>
+            <Route path="/payment" component={Payment} />
             <Route path="/register" component={Register} />
           </Switch>
         </div>
---
+++ Logic/MyPresentation/react-app/src/components/EditableItem.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import Image from "./images/shirt.jpg";
+import "./Product.css";
+class EditableItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: this.props.name,
+      available: this.props.available,
+      amount: this.props.amount,
+      price: this.props.price,
+    };
+  }
+  render() {
+    return (
+      <div className="col-md-3">
+        <figure className="itemside mb-4">
+          <div className="right-aside">
+            <img src={Image} className="img-sm" />
+          </div>
+          <figcaption className="info align-self-center">
+            <h4 classNameName="center">{this.state.name}</h4>
+            <h4>{this.state.available}</h4>
+            <h4>{this.state.amount}</h4>
+            <h4>{this.state.price}</h4>
+            <button className="btn btn-outline-primary btn-sm">
+              {" "}
+              Edit Item <i className="fa fa-edit"></i>
+            </button>
+          </figcaption>
+        </figure>
+      </div>
+    );
+  }
+}
+export default EditableItem;
