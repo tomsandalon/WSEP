b0d230ccbc64e70c37a58ba89552bb083abd527f
Mark Oulitin <fragoric@gmail.com>
2021-05-08 22:50:57 +0300

    Server API tests - Login and Registration

--- Logic/Communication/Client.test.ts
+++ Logic/Communication/Client.test.ts
@@ -1,41 +1,116 @@
 import {sleep} from "async-parallel";
-import {app} from "./Server";
+import {app, server} from "./Server";
 import nock from "nock";
+import {sid} from "./Config/Config";
+import {afterEach, beforeEach} from "mocha";
 const fs = require('fs')
 const https = require('https');
-const request = require('request');
+const request = require('supertest');
 const chai = require('chai');
+const expect = require('chai').expect;
 const chaiNock = require('chai-nock');
 chai.use(chaiNock);
-describe('', () => {
-    it('',  async () =>{
-        // nock('https://localhost:8000/')
-        //     .get('/home').
-        //     ;
-        // console.log(res)
-            // .send({name: "Charlie", age: "9", breed: "Pomerian"})
-        // let sid = -1;
-        // const options = {
-        //     method: 'GET',
-        //     port: 8000,
-        //     hostname: 'localhost',
-        //     path: '/home'
-        // };
-        // const happy = () => new Promise((resolve) =>{
-        //     https.request(options, (res: any) => {
-        //         console.log("Here")
-        //         console.log(res.headers);
-        //     })
-        // })
-        // request('https://localhost:8000/home', (err: any, res: any, body: any) => {
-        //     console.log(err);
-        //     console.log(res);
-        //     console.log(body);
-        // })
-        // // const sad = (v, ms) => new Promise((_, reject) => setTimeout(() => reject(v), ms))
-        // //
-        // Promise.all([happy()])
-        //     .then(console.log) // [{ "status":"fulfilled", "value":"happy" }, { "status":"rejected", "reason":"sad" }]
+describe('Server API tests', () => {
+    const cookie_prefix = 'SID=';
+    let sess_id = -1;
+    beforeEach((done) =>{
+        request(app).get('/guest')
+            .expect(200)
+            .then((res: any) =>{
+                sess_id = res.headers['set-cookie'].find((cookie: any) => cookie.startsWith(sid))
+                    .split(';')[0]
+                    .split('=')[1];
+                done();
+            })
+            .catch((err: any) => done(err));
     })
-
+    afterEach((done) => {
+        server.close();
+        done();
+    })
+    describe('Login tests', () => {
+        it('Login successful',  (done) =>{
+            request(app).post('/login')
+                .set('Cookie', cookie_prefix + sess_id)
+                .send({
+                    email: "Tomer@gmail.com",
+                    password: "123456"
+                })
+                .expect(200, done);
+        })
+        it('Login unsuccessful - Bad password',  (done) =>{
+            request(app).post('/login')
+                .set('Cookie', cookie_prefix + sess_id)
+                .send({
+                    email: "Tomer@gmail.com",
+                    password: "12345"
+                })
+                .expect(401, done);
+        })
+        it('Login unsuccessful - Bad email',  (done) =>{
+            request(app).post('/login')
+                .set('Cookie', cookie_prefix + sess_id)
+                .send({
+                    email: "Tomer@gmail.com",
+                    password: "12345"
+                })
+                .expect(401, done);
+        })
+        it('Login unsuccessful - user already logged',  (done) =>{
+            request(app).post('/login')
+                .set('Cookie', cookie_prefix + sess_id)
+                .send({
+                    email: "Tomer@gmail.com",
+                    password: "12345"
+                })
+                .expect(401, done);
+        })
+    })
+    describe('Registration tests', () => {
+        it('Registration unsuccessful - bad email - no @',  (done) =>{
+            request(app).post('/registration')
+                .set('Cookie', cookie_prefix + sess_id)
+                .send({
+                    email: "Nethanelgmail.com",
+                    password: "123456"
+                })
+                .expect(400, done);
+        })
+        it('Registration unsuccessful - bad email - no dot',  (done) =>{
+            request(app).post('/registration')
+                .set('Cookie', cookie_prefix + sess_id)
+                .send({
+                    email: "Nethanel@gmailcom",
+                    password: "123456"
+                })
+                .expect(400, done);
+        })
+        it('Registration unsuccessful - email empty',  (done) =>{
+            request(app).post('/registration')
+                .set('Cookie', cookie_prefix + sess_id)
+                .send({
+                    email: "",
+                    password: "123456"
+                })
+                .expect(400, done);
+        })
+        it('Registration unsuccessful - password empty',  (done) =>{
+            request(app).post('/registration')
+                .set('Cookie', cookie_prefix + sess_id)
+                .send({
+                    email: "Nethanel@gmail.com",
+                    password: ""
+                })
+                .expect(400, done);
+        })
+        it('Registration successful',  (done) =>{
+            request(app).post('/registration')
+                .set('Cookie', cookie_prefix + sess_id)
+                .send({
+                    email: "Nethanel@gmail.com",
+                    password: "123456"
+                })
+                .expect(200, done);
+        })
+    })
 });
