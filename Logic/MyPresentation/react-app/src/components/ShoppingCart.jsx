28975266c7eafa1f433c455dd6572827f0529467
Lior Pevzner <liorpev@post.bgu.ac.il>
2021-05-08 18:22:02 +0300

    Cart split into baskets

--- Logic/MyPresentation/react-app/src/components/ShoppingCart.jsx
+++ Logic/MyPresentation/react-app/src/components/ShoppingCart.jsx
@@ -1,7 +1,5 @@
 import React, {Component} from 'react';
-import Image from './images/cart.png';
-import BasketItem from './BasketItem';
-
+import Basket from './Basket';
 class ShoppingCart extends Component {
 	state = {
 		cart:[],
@@ -50,54 +48,26 @@ class ShoppingCart extends Component {
 				}
 			})
 	  }
+	  render(){
+		  return(
+				<div className="row">
+					{this.state.cart.map(basket =>
+					<Basket basket_products={basket.products} selected_basket={basket}></Basket>
+					)}
+				</div>
+		  );
+	  }
+	}
 	// calculateTotalPrices() {
 	// 	let price = 0;
 	// 	this.state.cart.forEach(basket => 
 	// 		basket.products.forEach(product => price+=(product.amount*product.product._base_price))
-	// 		)
+	// 		)	//<Basket selected_basket={basket} basket_products={basket.products}></Basket>)}
 	// 	this.setState({price:price})
 	// }
-	toggle(){
-		this.setState({visible:!this.state.visible, errorMsg:''})
-	}
-    render() {
-        return(
-            <div className="row">
-		<main className="col-md-9">
-            {this.state.cart.map((basket,index) =>(
-			basket.products.map(product => 
-				<BasketItem img={Image} shop_id={basket.shop_id} amount={product.amount} basket_id={basket.basket_id} shop_name={basket.shop_name} product_id={product.product._product_id} item_name={product.product._name} price={product.product._base_price}/>
-				)
-            ))}
-		</main> 
-		<aside className="col-md-3">
-			<div className="card">
-			<div className="card-body">
-				<div className="input-group mb-3">
-					<input type="text" className="form-control" name="" placeholder="Promo Code"/>
-					<span className="input-group-append"> 
-						<button className="btn btn-primary">Ok</button>
-					</span>
-				</div>
-				<dl className="dlist-align">
-				  <dt>Subtotal:</dt>
-				  <dd className="text-right">{this.state.price}</dd>
-				</dl>
-				<dl className="dlist-align">
-				  <dt>Discount:</dt>
-				  <dd className="text-right text-danger">0%</dd>
-				</dl>
-				<dl className="dlist-align">
-				  <dt>Grand Total:</dt>
-				  <dd className="text-right text-dark"><strong>{this.state.price}</strong></dd>
-				</dl>
-				<button href="#" className="btn btn-primary btn-block"> Purchase </button>
-				<p className="small my-3 text-muted">Some extra informative text  can be placed here as dummy text will be replaced</p>
-			</div> 
-			</div> 
-		</aside> 
-	</div>
-        );
-    }
-}
+	// toggle(){
+	// 	this.setState({visible:!this.state.visible, errorMsg:''})
+	// }
+    
+	
 export default ShoppingCart;
