a99d259c477ca26c528f4590568917b60d4177ec
Mark Oulitin <fragoric@gmail.com>
2021-05-09 13:22:44 +0300

    Working on AssignManager.ts

--- Logic/Communication/Tests/Composite/Shop/AssignManager.ts
+++ Logic/Communication/Tests/Composite/Shop/AssignManager.ts
@@ -1,15 +1,43 @@
 import {app} from "../../../Server";
 const expect = require('chai').expect;
 import {cookie_prefix, SessionTest} from "../../Setup";
-import {beforeEach} from "mocha";
+import {before, beforeEach} from "mocha";
 import {
     BadRequest,
-    OK,
-    route_shop_management,
+    OK, route_guest, route_login, route_register,
+    route_shop_management, sid,
 } from "../../../Config/Config";
 const request = require('supertest');
 
 describe('Add Manager to Shop tests', () => {
+    before((done => {
+        request(app).post(route_register)
+            .set('Cookie', cookie_prefix + SessionTest.sess_id)
+            .send({
+                email: "TomerNagar@gmail.com",
+                password: "123456"
+            })
+            .expect(400, done);
+
+        request(app).get(route_guest)
+            .expect(200)
+            .then((res: any) =>{
+                SessionTest.sess_id = res.headers['set-cookie'].find((cookie: any) => cookie.startsWith(sid))
+                    .split(';')[0]
+                    .split('=')[1];
+                console.log(SessionTest.sess_id)
+                done();
+            })
+            .catch((err: any) => done(err));
+
+        request(app).post(route_login)
+            .set('Cookie', cookie_prefix + SessionTest.sess_id)
+            .send({
+                email: "TomAndSons@gmail.com",
+                password: "123456"
+            })
+            .expect(OK, done);
+    }))
     beforeEach(() =>{
         expect(SessionTest.sess_id).not.equal('')
     })
@@ -18,7 +46,7 @@ describe('Add Manager to Shop tests', () => {
             .set('Cookie', cookie_prefix + SessionTest.sess_id)
             .send({
                 shop_id: 1,
-                app: 0
+                appointee_user_email: ""
             })
             .expect(BadRequest, done);
     })
