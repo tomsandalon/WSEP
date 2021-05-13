f2339022fed7994d5b140f153e44037dc2383cae
Mark Oulitin <fragoric@gmail.com>
2021-05-13 17:55:44 +0300

    Added assign manager and owner routes

--- Logic/Communication/User/Registered/Ownership.ts
+++ Logic/Communication/User/Registered/Ownership.ts
@@ -2,32 +2,33 @@ import {service, Session, sid} from "../../Config/Config";
 const express = require('express');
 const router = express.Router();
 module.exports = router;
-router.get('/', (request: any, response: any) => {
+
+router.post('/assign/owner', (request: any, response: any) => {
     const user_id = Session.sessions[request.cookies[sid]];
     if (user_id == undefined) {
         response.status(404);
         response.send('Bad session id')
         response.end()
     }
-    response.setHeader("Content-Type", "application/json");
-    const result = service.getAllShops(user_id);
-    console.log(result)
-    response.json(result)
-    response.end();
-})
-router.put('/', (req: any, res: any) => {
-    //TODO perform logout
-    res.send("My first server!\n" + req.url + "\n:D")
+    const result = service.appointOwner(user_id, request.body.shop_id, request.body.email);
+    if (typeof result === 'string') {
+        response.status(400);
+        response.setHeader("Content-Type", "text/html");
+        response.send(result);
+    } else {
+        response.status(200);
+        response.end();
+    }
 })
 
-router.post('/', (request: any, response: any) => {
+router.post('/assign/manager', (request: any, response: any) => {
     const user_id = Session.sessions[request.cookies[sid]];
     if (user_id == undefined) {
         response.status(404);
         response.send('Bad session id')
         response.end()
     }
-    const result = service.addItemToBasket(user_id, request.body.product_id, request.body.shop_id, request.body.amount);
+    const result = service.appointManager(user_id, request.body.shop_id, request.body.email);
     if (typeof result === 'string') {
         response.status(400);
         response.setHeader("Content-Type", "text/html");
