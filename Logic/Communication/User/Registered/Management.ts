6808b93093499507e529981941bbaed9d178a378
Mark Oulitin <fragoric@gmail.com>
2021-05-14 22:24:38 +0300

    Added route to show shop of manager

--- Logic/Communication/User/Registered/Management.ts
+++ Logic/Communication/User/Registered/Management.ts
@@ -1,4 +1,4 @@
-import {OK, ServerNotFound, service, Session, sid} from "../../Config/Config";
+import {OK, ServerNotFound, service, Session, sid, Unauthorized} from "../../Config/Config";
 const express = require('express');
 const router = express.Router();
 module.exports = router;
@@ -10,10 +10,16 @@ router.get('/', (request: any, response: any) => {
         response.end()
         return
     }
-    response.setHeader("Content-Type", "application/json");
-    response.status(OK)
-    const result = service.getAllShops(user_id);
-    response.json(result)
+    const result = service.displayStaffInfo(user_id, request.body.shop_id);
+    if (typeof result == 'string'){
+        response.status(Unauthorized)
+        response.setHeader("Content-Type", "text/html");
+        response.send(result)
+    } else {
+        response.status(OK)
+        response.setHeader("Content-Type", "application/json");
+        response.json(result)
+    }
     response.end();
 })
 router.put('/', (req: any, res: any) => {
