561f5bcb5f9d54736d17081d1ca906845838a98d
Mark Oulitin <fragoric@gmail.com>
2021-05-23 21:15:39 +0300

    Added initial setup of database

---
+++ Logic/DataAccess/DB.config.js
@@ -0,0 +1,40 @@
+const knex = require('knex');
+const db_configurations = require('./knexfile');
+const db = knex(db_configurations.development);
+exports.db = db;
+exports.builder = db.schema;
+const defs = require('./Tables');
+const config = [
+    defs.purchase_type,
+    defs.permission,
+    defs.user,
+    defs.shop,
+    defs.product,
+    defs.purchase,
+    defs.basket,
+    defs.offer,
+    defs.rates,
+    defs.available,
+    defs.owns,
+    defs.manages,
+    defs.notification,
+    defs.purchase_condition_type,
+    defs.purchase_condition,
+    defs.purchase_simple_condition,
+    defs.purchase_composite_condition,
+    defs.purchase_condition_operator,
+    defs.purchase_comprised,
+    defs.purchase_condition_allowed_in,
+    defs.purchase_simple_condition_type_of,
+    defs.discount,
+    defs.discount_operator,
+    defs.discount_allowed_in,
+    defs.discount_conditional,
+    defs.discount_simple,
+    defs.discount_composite,
+    defs.discount_comprised_composite,
+    defs.discount_comprised_conditional,
+    defs.discount_condition_type,
+    defs.discount_conditional_type_of,
+];
+exports.config = config;
