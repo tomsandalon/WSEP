9ddf77c1dd5411f512e0b322e02676422fcac97c
yehnet <yehnet@post.bgu.ac.il>
2021-05-31 21:36:04 +0300

    integration start

--- Tests/Developer/Unit/ShoppingBasket/ShoppingBasket.tests.ts
+++ Tests/Developer/Unit/ShoppingBasket/ShoppingBasket.tests.ts
@@ -61,16 +61,4 @@ describe("Purchase test", () => {
         const result = user.purchaseBasket(shop.shop_id,"1234-Israel-Israeli");
         expect(typeof result == "string" && result.includes("doesn't have enough in stock for this purchase")).to.be.true
     });
-    it('Two concurrent purchases or item which cause lack to stock', () => {
-        const shop: ShopImpl = new ShopImpl("Tom@gmail.com", "12345-TOM-SAND", "Best local shop in the negev", "Negev", "Tom and sons");
-        const user1: User = new UserImpl();
-        const user2: User = new UserImpl();
-        shop.addItem("Tom@gmail.com", "GTX", "GPU", 1000, ["GPU", "HW"], 1000);
-        user1.addToBasket(shop.inventory, getNewItem(shop.inventory), 999);
-        user2.addToBasket(shop.inventory, getNewItem(shop.inventory), 999);
-        let result = user1.purchaseBasket(shop.shop_id,"1234-Israel-Israeli");
-        expect(typeof result == "string").to.be.false
-        result = user2.purchaseBasket(shop.shop_id,"1234-Israel-Israeli");
-        expect(typeof result == "string").to.be.true
-    });
 })
