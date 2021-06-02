73233c92c4d83074c94a8a51d25a7090e4c47e89
Tomer <nagarcode@gmail.com>
2021-06-02 09:47:12 +0300

    Discounts displayed

--- Logic/MyPresentation/react-app/src/pages/ManagersStore.js
+++ Logic/MyPresentation/react-app/src/pages/ManagersStore.js
@@ -4,6 +4,9 @@ import OwnersBlock from "../components/OwnersBlock";
 import { Link, useParams, useHistory } from "react-router-dom";
 import useFetch from "../useFetch";
 import { React } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Divider } from "@material-ui/core";
+import DiscountsBlock from "../components/DiscountsBlock";
 
 const ManagersStore = () => {
   const { storeID, name } = useParams();
@@ -13,17 +16,27 @@ const ManagersStore = () => {
     itemsIsPending,
     error: itemsError,
   } = useFetch(`/user/shop?shop_id=${storeID}`);
+
   const {
     data: staff,
     staffIsPending,
     staffError,
   } = useFetch(`/user/shop/management?shop_id=${storeID}`);
   const parsedStaff = JSON.parse(staff);
+
   const {
     data: myEmail,
     detailsIsPending,
     detailsError,
   } = useFetch(`/user/details`);
+
+  const {
+    data: unparsed_discounts,
+    discountsIsPending,
+    discountsError,
+  } = useFetch(`/user/shop/discount?shop_id=${storeID}`);
+  const discounts = JSON.parse(unparsed_discounts);
+
   const isUser = () => {
     const requestOptions = {
       method: "GET",
@@ -43,45 +56,61 @@ const ManagersStore = () => {
       }
     });
   };
+
   isUser();
+
   return (
     <div className="row">
-      {parsedStaff && (
-        <div className="col-xl-4">
-          <h3>Store Owners:</h3>
-          {myEmail && (
-            <OwnersBlock
-              owners={parsedStaff.owners}
-              error={staffError}
-              isPending={staffIsPending}
-              myEmail={myEmail.email}
-              storeID={storeID}
-            />
-          )}
-          <Link to={`/addmanager/${storeID}/owner/${name}`}>
-            <button className="btn btn-outline-primary btn-sm ">
-              Add Owner <i className="fa fa-plus"></i>
-            </button>
-          </Link>
-          <h3>Store Managers:</h3>
-          {myEmail && (
-            <ManagersBlock
-              managers={parsedStaff.managers}
-              error={staffError}
-              isPending={staffIsPending}
-              storeID={storeID}
-              storeName={name}
-              myEmail={myEmail.email}
-            />
-          )}
-          <Link to={`/addmanager/${storeID}/manager/${name}`}>
-            <button className="btn btn-outline-primary btn-sm ">
-              Add Manager <i className="fa fa-plus"></i>
-            </button>
-          </Link>
-        </div>
-      )}
-      <div className="col-xl-8">
+      <div className="col-3">
+        <output>
+          <div className="card">
+            {parsedStaff && (
+              <article className="management">
+                <div className="card-body">
+                  <form className="mb-3">
+                    <ul>
+                      <h3>Store Owners:</h3>
+                      {myEmail && (
+                        <OwnersBlock
+                          owners={parsedStaff.owners}
+                          error={staffError}
+                          isPending={staffIsPending}
+                          myEmail={myEmail.email}
+                          storeID={storeID}
+                        />
+                      )}
+                      <Link to={`/addmanager/${storeID}/owner/${name}`}>
+                        <button className="btn btn-outline-primary btn-sm ">
+                          Add Owner <i className="fa fa-plus"></i>
+                        </button>
+                      </Link>
+                      <hr></hr>
+                      <h3>Store Managers:</h3>
+                      {myEmail && (
+                        <ManagersBlock
+                          managers={parsedStaff.managers}
+                          error={staffError}
+                          isPending={staffIsPending}
+                          storeID={storeID}
+                          storeName={name}
+                          myEmail={myEmail.email}
+                        />
+                      )}
+                      <Link to={`/addmanager/${storeID}/manager/${name}`}>
+                        <button className="btn btn-outline-primary btn-sm ">
+                          Add Manager <i className="fa fa-plus"></i>
+                        </button>
+                      </Link>
+                    </ul>
+                  </form>
+                </div>
+              </article>
+            )}
+          </div>
+        </output>
+      </div>
+
+      <div className="col-8">
         <div className="store-view" key={storeID}>
           <h3 className="mb-0">{name}</h3>
           <Link to={`/addproduct/${storeID}/${name}`}>
@@ -93,6 +122,24 @@ const ManagersStore = () => {
           {itemsIsPending && <div>Loading...</div>}
           <ItemsBlock id={storeID} storeItems={storeItems}></ItemsBlock>
         </div>
+        <hr></hr>
+        <div classname="store-view">
+          <h3>Discounts:</h3>
+          <Link to={`/adddiscount/${storeID}/${name}`}>
+            <button className="btn btn-outline-primary btn-sm ">
+              Add Discount <i className="fa fa-plus"></i>
+            </button>
+          </Link>
+          {discounts && (
+            <DiscountsBlock
+              storeID={storeID}
+              error={discountsError}
+              isPending={discountsIsPending}
+              discounts={discounts}
+            />
+          )}
+        </div>
+        {/* </div> */}
       </div>
     </div>
   );
