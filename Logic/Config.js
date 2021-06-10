100d9a7f3e63edc390c26247cd53ca83466a1590
yehnet <yehnet@post.bgu.ac.il>
2021-06-10 14:37:48 +0300

    Moving to laptop

---
+++ Logic/Config.js
@@ -0,0 +1,11 @@
+exports.server_host = 'localhost'
+exports.server_port = 8000
+exports.client = 'mysql'
+exports.connection =  {
+    host: '127.0.0.1',
+    database: 'wsep',
+    user:     'Mark',
+    password: '123456',
+    charset: 'utf8'
+}
+exports.paymentAndDeliveryServices = 'https://cs-bgu-wsep.herokuapp.com/'
