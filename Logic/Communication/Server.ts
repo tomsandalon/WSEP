caab0bb2498dbf31cc23c298b2bba74e4ba16a3c
Mark Oulitin <fragoric@gmail.com>
2021-06-11 18:05:57 +0300

    Moved Config.js to Config json and added tests

--- Logic/Communication/Server.ts
+++ Logic/Communication/Server.ts
@@ -29,8 +29,8 @@ const socket_io = require('socket.io');
 const fs = require('fs')
 const path = require('path');
 const express = require('express');
-// const expressWs = require('express-ws');
 const cookieParser = require('cookie-parser');
+const {loadConfig, isLoaded} = require('../Config');
 export const app = express();
 //initialize a https server
 export const server = https.createServer(options, app);
@@ -43,29 +43,35 @@ export const io = socket_io(server,
       });
 configWebSocket(io)
 //start our server
-server.listen( port,() => {
-    console.log(`Server is running on port ${port}`);
-})
-// expressWs(app, server);
-app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use(route_guest, require('./User/Guest'))
-app.use(route_login, require('./User/Registered/Login'));
-app.use(route_logout, require('./User/Registered/Logout'))
-app.use(route_register, require('./User/Register'));
-app.use(route_cart, require('./User/Cart'));
-app.use(route_purchase, require('./User/Purchase'));
-app.use(route_home, require('./Home/Home'));
-app.use(route_filter, require('./Home/Filter'));
-app.use(route_shop_management, require('./User/Registered/Management'));
-app.use(route_shop_ownership, require('./User/Registered/Ownership'));
-app.use(route_shop_manage_product, require('./User/Registered/Product'))
-app.use(route_shop, require('./User/Registered/Shop'));
-app.use(route_admin, require('./User/Registered/Admin'));
-app.use(route_user_management, require('./User/Registered/User'));
-app.use(route_shop_policy, require('./User/Registered/Policy'));
-app.use(route_shop_discount, require('./User/Registered/Discount'));
-//* For debug TODO delete this
+const initServer = () => {
+    service.init().then(_ => service.initData()).then(_ => console.log('Ready'));
 
-service.init().then(_ => service.initData()).then(_ => console.log('Ready'));
+    server.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    })
+    app.use(cookieParser());
+    app.use(express.json());
+    app.use(express.urlencoded({extended: false}));
+    app.use(route_guest, require('./User/Guest'))
+    app.use(route_login, require('./User/Registered/Login'));
+    app.use(route_logout, require('./User/Registered/Logout'))
+    app.use(route_register, require('./User/Register'));
+    app.use(route_cart, require('./User/Cart'));
+    app.use(route_purchase, require('./User/Purchase'));
+    app.use(route_home, require('./Home/Home'));
+    app.use(route_filter, require('./Home/Filter'));
+    app.use(route_shop_management, require('./User/Registered/Management'));
+    app.use(route_shop_ownership, require('./User/Registered/Ownership'));
+    app.use(route_shop_manage_product, require('./User/Registered/Product'))
+    app.use(route_shop, require('./User/Registered/Shop'));
+    app.use(route_admin, require('./User/Registered/Admin'));
+    app.use(route_user_management, require('./User/Registered/User'));
+    app.use(route_shop_policy, require('./User/Registered/Policy'));
+    app.use(route_shop_discount, require('./User/Registered/Discount'));
+}
+
+if(isLoaded() || loadConfig()){
+    initServer();
+} else {
+    console.log("Server cannot read config file\nAborting...")
+}
